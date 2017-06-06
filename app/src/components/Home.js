0c6935a6c3f79bcc7968e978f6a91ecd952dbf93
Anton <astandrik@gmail.com>
2017-06-06 16:54:26 +0300

    win boxes

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import MatchForm from "./MatchForm";
 import "./Home.css";
 
 export default class Home extends  React.Component{
   render() {
     const props = this.props;
     let list = [];
-    if(props.matches) list = props.matches.map(x => <div key={x.id}><span>1: {x.name1}</span> <span>2: {x.name2}</span> </div>)
+    if(props.matches) list = props.matches
+                             .map(x => <div key={x.id}><MatchForm name1={x.name1} name2={x.name2}/></div>);
     return (
       <div className="flexV full-height">
         <div className="header">
@@ -13,7 +15,9 @@ export default class Home extends  React.Component{
         </div>
         <div className="workSpace">
           <button className="add_button" onClick={props.addMatch}/>
-          {list}
+          <div className="who-win-list">
+            {list}
+          </div>
         </div>
       </div>
     );
---
+++ app/src/components/MatchForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class MatchForm extends React.Component {
+  render() {
+    const props = this.props;
+    return (
+      <div className="match-form">
+        <div className="left">
+        {props.name1}
+        </div>
+        <div className="right">
+        {props.name2}
+        </div>
+      </div>
+    )
+  }
+}
